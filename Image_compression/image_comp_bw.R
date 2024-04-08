dedfb5791c4c1750b946ba1a8f78746ab25c1513
Jamie Harris <jamie@harrishomesite.com>
2024-04-08 20:14:40 +0100

    Update image_comp_bw.R

--- Image_compression/image_comp_bw.R
+++ Image_compression/image_comp_bw.R
@@ -1,8 +1,4 @@
 library(jpeg)
-library(grid)
-library(SpatialPack)
-
-
 
 #Load in photo
 file_name <- 'cheetah'
@@ -14,27 +10,25 @@ X <- t(BW)
 
 #Center and scale image
 mu <- colMeans(X)
-sigma <- apply(X, 2, sd)
-X <- (X-mu)/sigma
+X <- (X-mu)
 
 #Compute SVD
 SVD<- svd(X)
-U <- SVD$u
-D <- diag(SVD$d)
-phi <- SVD$v
+Theta <- SVD$u
+A <- diag(SVD$d)
+U <- SVD$v
 
 
-#Compute q approx of Z
-q=c(10, 20, 50, 100, 500)
-for(i in q){
-  Z_unscaled <- U[,1:i] %*% D[1:i,1:i] %*% t(phi[,1:i])
-  Z <- (Z_unscaled *sigma) + mu
+#Compute k approx of X
+for(k in c(10, 20, 50, 100, 500)){
+  X_tilde_unscaled <- Theta[,1:k] %*% A[1:k,1:k] %*% t(U[,1:k])
+  X_tilde <- (X_tilde_unscaled) + mu
   
   
   #Write image to storage
-  image <- t(Z)
+  image <- t(X_tilde)
   writeJPEG(image, paste('Image_compression/', file_name, '/BW/', file_name,"_",
-                         i, '_BW_comp.jpeg',sep=''), quality=1)
+                         k, '_BW_comp.jpeg',sep=''), quality=1)
   
 }
 
